04eba74355763be5ea9fe319a0d17513efd7aee1
Emerson Costa <emersonjscosta@gmail.com>
2025-01-22 21:20:23 -0300

    Iniciando a construção do projeto To do List

---
+++ app/src/main/java/com/emerson/todolist/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.emerson.todolist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+}
